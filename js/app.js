41fc631a5926bc77aa9dfd316e746009b72dfd11
Tim Campbell <tim.campbell@btinternet.com>
2018-08-06 19:41:41 +0100

    fix: Reset stars fix and multiple card click fix
    Corrected the problem where the stars would not reset when restarting
    the game.  Also corrected the problem where the user could click more
    than 2 cards, whilst the app was processing whether it was a match or
    not.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+let time, interval, moves;
+//const stars = document.querySelectorAll('.stars li i');
+//let interval;
 /*
  * Create a list that holds all of your cards
  */
@@ -11,6 +14,8 @@ let cards = ['fa-diamond', 'fa-diamond',
             'fa-bicycle', 'fa-bicycle',
             'fa-leaf', 'fa-leaf',
             'fa-bomb', 'fa-bomb'];
+
+let stars = document.querySelectorAll('.stars li i');
 
 
 /*
@@ -43,15 +48,32 @@ function generateCard(card) {
 //generate the cards
 function initGame() {
   const deck = document.querySelector('.deck');
+
   let cardHTML = shuffle(cards).map(function(card) {
     return generateCard(card);
   });
+
+  moves = 0;
+  //moveCounter.innerText = moves;
   deck.innerHTML = cardHTML.join('');
+
+  //Set time to zero
+  time = 0;
+  //let interval;
+  const timer = document.querySelector('.timer');
+
+  //set initial timer text
+  timer.innerText = `${time} seconds`;
+
+  resetStars();
+  startTimer();
+  gamePlay();
+
 }
+
 
 //start the game
 initGame();
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -66,50 +88,185 @@ initGame();
 
 
 
+function gamePlay() {
+
 
 //Select all the cards
 const allCards = document.querySelectorAll('.card');
 //Define array for the open cards
 let openCards = [];
 
+//Set number of moves to zero
+//let moves = 0;
+const moveCounter = document.querySelector('.moves');
+moveCounter.innerText = moves;
+
 //Loop through the cards and add an event listener for a mouse click to flip over a card
-allCards.forEach(function(card) {
-  card.addEventListener('click', function(e) {
 
-    if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
-        openCards.push(card);
-        card.classList.add('open', 'show');
-        console.log('Open cards:', openCards.length);
+  allCards.forEach(function(card) {
+    card.addEventListener('click', function(e) {
 
+      if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
+          openCards.push(card);
+          card.classList.add('open', 'show');
+          console.log(openCards.length);
+          //console.log('Open cards:', openCards.length);
 
+          //start timer on first card flip
+          //if (openCards.length === 1) {
 
-        //only allows 2 cards to be flipped
-        if (openCards.length == 2) {
-          //check if cards match
-          if (openCards[0].dataset.card == openCards[1].dataset.card){
-            openCards[0].classList.add('match');
-            openCards[0].classList.add('open');
-            openCards[0].classList.add('show');
+          //}
 
-            openCards[1].classList.add('match');
-            openCards[1].classList.add('open');
-            openCards[1].classList.add('show');
+//PRevent more than 2 cards being selected at a time
+        //  if(openCards.length < 2) {
 
-            //clear array
-            openCards = [];
-          } else {
+            //only allows 2 cards to be flipped
+            if (openCards.length == 2) {
+              // Temporarily prevent any further card selection, to prevent
+              // the case where you can actually click 3 cards whilst
+              // processing the results.
+              document.body.style.pointerEvents='none';
+              //check if cards match
+              if (openCards[0].dataset.card == openCards[1].dataset.card){
+                openCards[0].classList.add('match');
+                openCards[0].classList.add('open');
+                openCards[0].classList.add('show');
 
-            //hides the cards if they don't match
-            setTimeout(function() {
-              openCards.forEach(function(card) {
-                card.classList.remove('open', 'show');
-              });
+                openCards[1].classList.add('match');
+                openCards[1].classList.add('open');
+                openCards[1].classList.add('show');
 
-              //clear array
-              openCards = [];
-            }, 1000);
-          }
+                //clear array
+                openCards = [];
+                // Allow cards to be selected again.
+                document.body.style.pointerEvents='auto';
+              } else {
+
+                //hides the cards if they don't match
+                setTimeout(function() {
+                  openCards.forEach(function(card) {
+                    card.classList.remove('open', 'show');
+                  });
+                  // Temporarily prevent cards being clicked
+                  document.body.style.pointerEvents='auto';
+
+                  //clear array
+                  openCards = [];
+                }, 1000);
+              }
+
+              //increment the number of moves
+              moves++;
+              moveCounter.innerText = moves;
+              starChange();
+              gameComplete();
+            }
+
+    //      } else {
+    //        return false;
+    //      }
         }
-      }
+      });
     });
+}
+
+//completed Game
+function gameComplete () {
+  let matchedCards = document.querySelectorAll('ul.deck li.match');
+
+  if (matchedCards.length == 16) {
+    //console.log('Game over!');
+    //window.alert("Game over!");
+    stopTimer();
+    showModal();
+  }
+};
+
+  //reset button
+  const restartBtn = document.querySelector('.restart');
+
+  restartBtn.addEventListener('click', function() {
+    stopTimer();
+    initGame();
+    //moves = 0;
+    //time = 0;
+    console.log('restart activated');
   });
+
+//function for displaying the message at the end of the game
+function showModal() {
+  let modal = document.querySelector('.modal');
+  let modalText = document.querySelector('.modal-content h2');
+
+  modalText.innerHTML = `Congratulations, you matched all the cards in ${time} seconds!`;
+  modal.classList.toggle('show-modal');
+}
+
+//modal play again button
+const playAgainBtn = document.querySelector('#modal-restart');
+let modal = document.querySelector('.modal');
+
+playAgainBtn.addEventListener('click', function() {
+  initGame();
+  //moves = 0;
+  //time = 0;
+  console.log('restart activated');
+  modal.classList.toggle('show-modal');
+});
+
+//modal ok button to close modal window
+const okBtn = document.querySelector('#modal-ok');
+
+
+okBtn.addEventListener('click', function() {
+  modal.classList.toggle('show-modal');
+})
+
+//timer functionionality, increment by 1 sec
+//time = 0;
+
+function incrementTimer() {
+const timer = document.querySelector('.timer');
+  //let time = 0;
+  time++;
+  timer.innerHTML = `${time} seconds`;
+  console.log(time);
+}
+
+//start timer
+//let interval;
+
+function startTimer() {
+  //let interval;
+  interval = setInterval(incrementTimer, 1000);
+}
+
+//stop timer
+
+function stopTimer() {
+  clearInterval(interval);
+}
+
+//change the star rating
+function starChange() {
+
+console.log('starChange accessed');
+console.log(`moves: ${moves}`);
+let stars = document.querySelectorAll('.stars li i');
+
+  if (moves > 10 && moves < 20) {
+    stars[2].classList.remove('active');
+  } else if (moves > 21 && moves < 30) {
+    stars[1].classList.remove('active');
+  } else if (moves > 31) {
+    stars[0].classList.remove('active');
+  }
+}
+
+function resetStars() {
+  let stars = document.querySelectorAll('.stars li i');
+    for (i=0; i < 3; i++) {
+      stars[i].classList.add('active');
+
+    }
+}
